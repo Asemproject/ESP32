3416e5a52283ec90646048720aab4446738947f3
Asem <162907069+Asemproject@users.noreply.github.com>
2026-01-17 11:19:01 +0700

    Refactor ESP32 board mapping for FluidNC compatibility
    Updated board mapping for ESP32 Mod by Asem, adjusting pin definitions and comments for compatibility with FluidNC.

--- main/boards/espduino_32_wemos_d1_r32_map.h
+++ main/boards/espduino_32_wemos_d1_r32_map.h
@@ -1,123 +1,85 @@
 /*
-  espduino-32_wemos_d1_r32_uno_map.h - An embedded CNC Controller with rs274/ngc (g-code) support
+  esp32_asem_mod_map.h - Map file for ESP32 Mod by Asem
+  Converted from FluidNC configuration
 
-  Driver code for ESPDUINO_32_WEMOS_D1_R32_UNO
-
-  Part of grblHAL
-
-  Copyright (c) 2020-2023 Terje Io
-
-  grblHAL is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  grblHAL is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with grblHAL. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-  This map is for relatively common ESP32 boards replicating the form factor of Arduino UNO.
-  This map allows use of such uno-compatible board with very popular
-  "Protoneer Arduino CNC shield" and is based on its pinout.
-  This makes perfect match for retrofiting older Arduino+grblHAL based machines
-  with 32b microcontroler capable of running grblHAL and providing few extra IO pins (eg. for modbus).
-
-  These boards are sold under several names, for instance:
-   + ESPDUINO-32  (USB type B)
-   + Wemos D1 R32 (Micro USB)
+  Part of grblHAL logic
 */
 
 #if N_ABC_MOTORS > 0
 #error "Axis configuration is not supported!"
 #endif
 
-#define BOARD_NAME "ESPDUINO-32 Wemos D1 R32"
+#define BOARD_NAME "ESP32 Mod by Asem"
+
+// -------------------------------------------------------------------------
+// STEPPER MOTOR PINS
+// -------------------------------------------------------------------------
 
 // Define step pulse output pins.
-#define X_STEP_PIN          GPIO_NUM_12
-#define Y_STEP_PIN          GPIO_NUM_27
+// FluidNC: X Step: gpio.2, Y Step: gpio.16, Z Step: gpio.25
+#define X_STEP_PIN          GPIO_NUM_2
+#define Y_STEP_PIN          GPIO_NUM_16
 #define Z_STEP_PIN          GPIO_NUM_25
 
-// Define step direction output pins. NOTE: All direction pins must be on the same port.
-#define X_DIRECTION_PIN     GPIO_NUM_14
-#define Y_DIRECTION_PIN     GPIO_NUM_26
-#define Z_DIRECTION_PIN     GPIO_NUM_33
+// Define step direction output pins.
+// FluidNC: X Dir: gpio.15, Y Dir: gpio.17, Z Dir: gpio.26
+#define X_DIRECTION_PIN     GPIO_NUM_15
+#define Y_DIRECTION_PIN     GPIO_NUM_17
+#define Z_DIRECTION_PIN     GPIO_NUM_26
 
 // Define stepper driver enable/disable output pin(s).
-#define STEPPERS_ENABLE_PIN GPIO_NUM_32
+// FluidNC: shared_stepper_disable_pin: gpio.13
+#define STEPPERS_ENABLE_PIN GPIO_NUM_13
 
-// Define homing/hard limit switch input pins and limit interrupt vectors.
-#define X_LIMIT_PIN         GPIO_NUM_17
-#define Y_LIMIT_PIN         GPIO_NUM_16
+// -------------------------------------------------------------------------
+// LIMIT SWITCH PINS
+// -------------------------------------------------------------------------
+
+// Define homing/hard limit switch input pins.
+// FluidNC: X Limit: gpio.27, Y Limit: gpio.14, Z Limit: gpio.4
+#define X_LIMIT_PIN         GPIO_NUM_27
+#define Y_LIMIT_PIN         GPIO_NUM_14
 #define Z_LIMIT_PIN         GPIO_NUM_4
 
-#define AUXOUTPUT0_PIN      GPIO_NUM_19 // Spindle PWM
-#define AUXOUTPUT1_PIN      GPIO_NUM_18 // Spindle enable
-#if !(MODBUS_ENABLE & MODBUS_RTU_ENABLED)
+// -------------------------------------------------------------------------
+// SPINDLE / COOLANT / PROBE (AUX I/O)
+// -------------------------------------------------------------------------
 
-// Define driver spindle pins
-#if DRIVER_SPINDLE_ENABLE & SPINDLE_PWM
-#define SPINDLE_PWM_PIN     AUXOUTPUT0_PIN
-#endif
-#if DRIVER_SPINDLE_ENABLE & SPINDLE_ENA
-#define SPINDLE_ENABLE_PIN  AUXOUTPUT1_PIN
-#endif
+/* NOTE: In your FluidNC config, Spindle, Coolant, and Probe are either
+   not defined or set to NO_PIN. 
+   
+   The original file used GPIO 19/18 for Spindle, but your config uses 
+   GPIO 19/18 for SPI (SD Card). Therefore, Spindle pins are disabled here 
+   to prevent conflict.
+*/
 
-// Define flood and mist coolant enable output pins.
-#if (COOLANT_ENABLE & COOLANT_MIST)
-#undef COOLANT_ENABLE
-#ifdef AUXOUTPUT2_PIN
-#define COOLANT_ENABLE COOLANT_FLOOD
-#else
-#define COOLANT_ENABLE 0
-#endif
-#endif
-#if (COOLANT_ENABLE & COOLANT_FLOOD)
-#ifdef AUXOUTPUT2_PIN
-#define COOLANT_FLOOD_PIN   AUXOUTPUT2_PIN
-#else
-#undef COOLANT_ENABLE
-#define COOLANT_ENABLE 0
-#endif
-#endif
+// #define SPINDLE_PWM_PIN     GPIO_NUM_NC
+// #define SPINDLE_ENABLE_PIN  GPIO_NUM_NC
+// #define COOLANT_FLOOD_PIN   GPIO_NUM_NC
+// #define COOLANT_MIST_PIN    GPIO_NUM_NC
+// #define PROBE_PIN           GPIO_NUM_NC
 
-#define AUXINPUT0_PIN       GPIO_NUM_39 // Probe
-
-// Define user-control controls (cycle start, reset, feed hold) input pins.
-#if CONTROL_ENABLE & CONTROL_HALT
-#define RESET_PIN           AUXINPUT1_PIN
-#endif
-#if CONTROL_ENABLE & CONTROL_FEED_HOLD
-#define FEED_HOLD_PIN       AUXINPUT2_PIN
-#endif
-#if CONTROL_ENABLE & CONTROL_CYCLE_START
-#define CYCLE_START_PIN     AUXINPUT3_PIN
-#endif
-
-#if PROBE_ENABLE
-#define PROBE_PIN           AUXINPUT0_PIN
-#endif
-
-#if MODBUS_ENABLE & MODBUS_RTU_DIR_ENABLED
-#define MODBUS_DIRECTION_PIN    GPIO_NUM_15
-#endif
-#endif
+// -------------------------------------------------------------------------
+// SD CARD / SPI PINS
+// -------------------------------------------------------------------------
 
 #if SDCARD_ENABLE
 // Pin mapping when using SPI mode.
-// With this mapping, SD card can be used both in SPI and 1-line SD mode.
-// Note that a pull-up on CS line is required in SD mode.
+// FluidNC SPI: miso: 19, mosi: 23, sck: 18
+// FluidNC SD: cs: 5, card_detect: 35
 #define SPI_MISO_PIN        GPIO_NUM_19
 #define SPI_MOSI_PIN        GPIO_NUM_23
 #define SPI_SCK_PIN         GPIO_NUM_18
 #define SD_CS_PIN           GPIO_NUM_5
+#define SD_CD_PIN           GPIO_NUM_35 
 #endif
+
+// -------------------------------------------------------------------------
+// CONTROL PINS
+// -------------------------------------------------------------------------
+
+// FluidNC: reset, feed_hold, cycle_start are all NO_PIN
+// Leaving these undefined or verifying config.h disables them.
 
 #if KEYPAD_ENABLE
 #error No free pins for keypad!
